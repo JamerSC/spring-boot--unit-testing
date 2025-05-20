e7bd04d8bb080f030e59a86ad62d462a24b0ddf7
Jamer Catalla <mr.catalla28@gmail.com>
2025-05-20 23:55:51 +0800

    Unit testing Isolation  of Student Service Impl - getAllStudents() & addStudent()

--- 01-spring-boot-unit-testing/README.md
+++ 01-spring-boot-unit-testing/README.md
@@ -1,5 +1,6 @@
 ### May 18, 2025
-### Learned & Practiced Unit & Integration testing using JUnit 5
+### Learned & Practiced Unit & Integration testing using JUnit 5 by AmigosCode
+https://www.youtube.com/watch?v=Geq60OVyBPg
 1. Tested basic junit
 2. Added Validation Dependency at POM.xml
 3. Added Logger Class
@@ -9,6 +10,9 @@
 * log.info("Student added successfully");
 * log.warn("Student with duplicate email tried to register");
 * log.error("Exception occurred while adding student", ex);
+
+### TESTING IN ISOLATION
+
 ### May 19, 2025
 4. Added H2 Database
 ### Unit Testing of Custom student repositories
@@ -16,3 +20,7 @@
 * Student Repository generate test for Custom Query selectEmailExist
 * Create Resource Folder in Test & add application.properties then configure using H2 db in memory access
 * Start unit testing & debugging.
+### May 19, 2025
+### Unit Testing of Custom student service
+### Select the Service Class then Ctrl + Shift + T then generate methods
+![img.png](docs/img.png)
---
+++ 01-spring-boot-unit-testing/src/test/java/com/jamersc/springboot/demo/service/StudentServiceImplTest.java
@@ -0,0 +1,76 @@
+package com.jamersc.springboot.demo.service;
+
+import com.jamersc.springboot.demo.model.Gender;
+import com.jamersc.springboot.demo.model.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceImplTest {
+
+    @Mock private StudentRepository studentRepository; // Mocks the StudentRepository using Mockito.
+    private StudentServiceImpl underTest;  // Initializes a new StudentServiceImpl before each test, injecting the mocked repository.
+    // This ensures each test gets a fresh, isolated service instance with a mock dependency.
+
+    @BeforeEach // run before each test
+    void setUp() {
+        underTest = new StudentServiceImpl(studentRepository); // fresh instance in student service
+    }
+
+    @Test
+    void canGetAllStudents() {
+        // when
+        underTest.getAllStudents(); // the actual method call on the service.
+
+        // then
+        verify(studentRepository).findAll(); // ensure the service calls findAll() on the repository
+
+        // ✅ Passes if findAll() was called once.
+        // ❌ Fails if it wasn’t called, was called multiple times, or some other method was called.
+    }
+
+    @Test
+    @Disabled
+    void getStudentById() {
+    }
+
+    @Test
+    void canAddStudent() {
+        // given
+        String mail = "john@mail.com";
+        Student student = new Student( // A sample Student object is created.
+                "John",
+                mail,
+                Gender.MALE
+        );
+
+        // when
+        underTest.addStudent(student); // is called on the service.
+
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+        // ArgumentCaptor<Student> is used to capture the exact Student object
+        // passed to studentRepository.save(...).
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        // verify(studentRepository).save(...) ensures the method was called.
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student); // confirms that the correct student instance was saved.
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+    }
+}
